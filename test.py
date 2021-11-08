67b84a1167e55d4a5d58a0ee765180510d77c37e
José de Jesús Andrade Santiago <314159735@pcpuma.acatlan.unam.mx>
2021-11-08 11:16:10 -0600

    Se agregó el guardado de un documento en heroku

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[6]:
 
 
 import pandas as pd
 df = pd.read_csv("test.csv", encoding = 'latin-1')
 print(df)
+df.to_csv('out.csv', index=False)  
 
 
 # In[ ]:
